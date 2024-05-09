46c33d8271815cb377902cf31123de1e6f2b4041
tao bruce <151921130+userecho1@users.noreply.github.com>
2024-05-10 03:40:18 +0800

    菜品分页查询dto

--- sky-pojo/src/main/java/com/sky/dto/DishPageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/DishPageQueryDTO.java
@@ -1,22 +1,28 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
+@ApiModel(description = "分页查询菜品数据模型")
 @Data
 public class DishPageQueryDTO implements Serializable {
 
+    @ApiModelProperty("菜品分页查询页数")
     private int page;
-
+    @ApiModelProperty("菜品分页查询每页数")
     private int pageSize;
-
+    @ApiModelProperty("菜品名")
     private String name;
 
     //分类id
+    @ApiModelProperty("菜品所属分类")
     private Integer categoryId;
 
     //状态 0表示禁用 1表示启用
+    @ApiModelProperty("菜品状态")
     private Integer status;
 
 }
