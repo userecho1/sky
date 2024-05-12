02c75fb960586cd7bc96eaf4e516352cae603493
tao bruce <151921130+userecho1@users.noreply.github.com>
2024-05-13 03:24:19 +0800

    添加购物车，debug了一些写错的地方

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,21 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+
+    @Update("update shopping_cart set number=#{number} where id=#{id}")
+    void update(ShoppingCart cart);
+
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    @Insert("INSERT INTO shopping_cart (name, image, user_id, dish_id, setmeal_id, dish_flavor, amount, create_time,number) " +
+            "VALUES (#{name}, #{image}, #{userId}, #{dishId}, #{setmealId}, #{dishFlavor}, #{amount}, #{createTime},#{number})")
+    void insert(ShoppingCart shoppingCart);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,71 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    @Override
+    @Transactional
+    public void addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+
+       ShoppingCart shoppingCart= new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+
+        List<ShoppingCart> shoppingCarts =shoppingCartMapper.list(shoppingCart);
+        if(shoppingCarts!=null&&shoppingCarts.size()>0){
+            ShoppingCart cart=shoppingCarts.get(0);
+            cart.setNumber(cart.getNumber()+1);
+            shoppingCartMapper.update(cart);
+        }else {
+            Long dishId=shoppingCartDTO.getDishId();
+            if(dishId!=null){
+                Dish dish=new Dish();
+                dish.setId(dishId);
+                Dish dish1=dishMapper.getOne(dish);
+
+                shoppingCart.setName(dish1.getName());
+                shoppingCart.setImage(dish1.getImage());
+                shoppingCart.setAmount(dish1.getPrice());
+
+
+            }else {
+                Long setmealId = shoppingCartDTO.getSetmealId();
+                Setmeal setmeal=setmealMapper.getById(setmealId);
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+
+
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.insert(shoppingCart);
+
+
+        }
+    }
+}
